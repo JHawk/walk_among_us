f0ee16043acb070796fad72d0d7ec95ddeeb9572
JHawk <josh.r.hawk@gmail.com>
2014-01-17 15:11:31 -0500

    some form of controls

---
+++ js/Start.js
@@ -0,0 +1,79 @@
+function start() {
+
+  var scene = new THREE.Scene();
+  var width = window.innerWidth, height = window.innerHeight;
+
+  // var camera = new THREE.OrthographicCamera( width / - 2, width / 2, height / 2, height / - 2, 1, 1000 );
+  // scene.add( camera );
+
+  var camera = new THREE.PerspectiveCamera( 75, width / height, 0.1, 1000 );
+  var renderer = new THREE.WebGLRenderer();
+  renderer.setSize( width, height );
+  document.body.appendChild( renderer.domElement );
+
+  var greenMaterial = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+  var redMaterial = new THREE.MeshBasicMaterial( { color: 0xff0000 } );
+  var whiteMaterial = new THREE.MeshBasicMaterial( { color: 0xffffff } );
+  var blueMaterial = new THREE.MeshBasicMaterial( { color: 0x0000ff } );
+
+  var gridWidth = 10, gridHeight = 10;
+
+  _(gridWidth).times(function(x) {
+    _(gridHeight).times(function(y) {
+      var cube;
+
+      var geometry = new THREE.CubeGeometry(1,1,2);
+
+      if (x >= gridWidth / 2 && y >= gridHeight / 2) // 1
+        {
+          cube = new THREE.Mesh( geometry, greenMaterial );
+        }
+        else if (x >= gridWidth / 2) // 2
+        {
+          cube = new THREE.Mesh( geometry, redMaterial );
+        }
+        else if (y >= gridHeight / 2) // 4
+        {
+          cube = new THREE.Mesh( geometry, blueMaterial );
+        }
+        else // 3
+        {
+          cube = new THREE.Mesh( geometry, whiteMaterial);
+        }
+      scene.add( cube );
+      cube.name = "cube-x:" + x + "-y:" + y;
+      cube.position.setX(x);
+      cube.position.setY(y);
+    });
+  });
+  
+  var self = this;
+
+  var degree = 0.0174532925;
+  var degree_45 = 45 * degree;
+
+  camera.position.z = 5;
+  camera.rotateY(-degree_45);
+  camera.rotateX(degree_45);
+  camera.rotateZ(-degree_45);
+
+  new SetupStats();
+
+  var controls;
+
+  // In the init() function :
+  controls = new THREE.Controls( camera );
+  // controls.addEventListener( 'change', render );
+
+  // // In the animate() function, before the call to render() :
+  // controls.update();
+
+  // Camera.orthoCamera.rotateOnAxis(X_AXIS, -90 * TO_RADIANS);
+
+  function render() {
+    requestAnimationFrame(render);
+    controls.update(1);
+    renderer.render(scene, camera);
+  }
+  render();
+}
