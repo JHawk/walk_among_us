fba369a5fe9cc5a21c7e0ec815ceb216d0149126
JHawk <josh.r.hawk@gmail.com>
2014-01-24 18:35:30 -0500

    made the minion model smaller than the wall blocks

--- js/meshes/Minion.js
+++ js/meshes/Minion.js
@@ -3,17 +3,22 @@ var meshes = meshes || {};
 meshes.Minion = function () {
   var self = this;
   self = _.extend(this, new style.Colors);
-  var size = 20;
+  self = _.extend(this, new helpers.Utils);
+  var size = 8;
+  var height = 30;
 
   this.create = function (x, y, color) {
     var material = new THREE.MeshLambertMaterial( { color: color } );
-    var geometry = new THREE.SphereGeometry(size, size, size + size / 2);
+    var geometry = new THREE.CylinderGeometry(5, size, height);
     
     var mesh = new THREE.Mesh(geometry, material);
     
-    mesh.position.setX(size * x);
-    mesh.position.setY(size * y);
-    mesh.position.setZ(size / 2);
+    mesh.position.setX(meshes.Wall.size * x);
+    mesh.position.setY(meshes.Wall.size * y);
+    mesh.position.setZ(height / 2);
+
+    mesh.rotation.x = self.radians(90);
+
     mesh.castShadow = true;
     return mesh;
   };
