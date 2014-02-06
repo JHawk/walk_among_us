fd4763a4bcfdd9a420846d09b65a2c4bbdd8dc93
JHawk <josh.r.hawk@gmail.com>
2014-02-05 22:16:26 -0500

    fixed a broken test

--- tests/jasmine/spec/controls/SpellsSpec.js
+++ tests/jasmine/spec/controls/SpellsSpec.js
@@ -2,13 +2,20 @@ describe("controls.Spells", function() {
 
   var Spells;
 
+  var boardSpy, mouseControlsSpy;
+
   beforeEach(function() {
+    boardSpy = jasmine.createSpyObj('board', ['spawnExplorer', 'spawnFighter']);
+    mouseControlsSpy = jasmine.createSpyObj('mouseControl', ['currentTarget']);
+
+    models.board = boardSpy;
+
     Spells = new controls.Spells();
   });
 
   describe("available", function () {
     it("will default to at least one", function() {
-      expect(Spells.available).not.toBeEmpty();
+      expect(Spells.actions).not.toBeEmpty();
     });
   });
 });    
